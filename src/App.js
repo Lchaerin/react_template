ecd64f5ba668db0b0f8d2f9f81a9c41babc3cac2
huijjj <hui0213@postech.ac.kr>
2021-07-28 13:30:37 +0900

    add: UI (step 1)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import './App.css';
+
 function App() {
   return (
     <div className="App">
+      <div className="block">-</div>
+      <div className="block">0</div>
+      <div className="block">+</div>
     </div>
   );
 }
