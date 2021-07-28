054334d525c9b15fda039e1be0f6799161715a57
huijjj <hui0213@postech.ac.kr>
2021-07-28 14:35:12 +0900

    add: store (step 5)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+import { useState } from 'react';
+
+const store = createStore(reducer);
+
 function App() {
+  const [ count, setCount ] = useState(store.getState().count);
+
   return (
     <div className="App">
       <div className="block">-</div>
-      <div className="block">0</div>
+      <div className="block">{count}</div>
       <div className="block">+</div>
     </div>
   );
